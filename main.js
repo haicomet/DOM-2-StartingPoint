942ad13e9391d7b74d57a7d2d4615ca5c8f430c7
Benjamin Ayala <benjaminayala@Benjamins-Mac-mini.local>
2025-06-20 15:41:35 -0400

    click on a single cell, changing its color to the currently selected color

--- main.js
+++ main.js
@@ -6,15 +6,6 @@ root.addEventListener("click", (event) => {
   console.log(event.target);
 });
 
-//Task 5
-const colorSelection = document.getElementById('color-select');//Selects element where user will choose colors
-const colorChoice = colorSelection.value;//Where user selects which color 
-
-colorSelection.addEventListener('change' , function(){
-  colorChoice = colorSelection.value;
-  console.log("User selected:", selectedColor);
-
-})
 
 //Task6
 const colorSelect = document.getElementById("color-select"); // Dropdown
