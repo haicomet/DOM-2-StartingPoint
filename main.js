633a362a9088031be15860d6000f25e7f6540df7
Benjamin Ayala <benjaminayala@Benjamins-Mac-mini.local>
2025-06-20 15:29:42 -0400

    Complete task 6: color cell on click

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ root.addEventListener("click", (event) => {
   console.log(event.target.tagName);
   console.log(event.target);
 });
+
+//Task 5
+const colorSelection = document.getElementById('color-select');//Selects element where user will choose colors
+const colorChoice = colorSelection.value;//Where user selects which color 
+
+colorSelection.addEventListener('change' , function(){
+  colorChoice = colorSelection.value;
+  console.log("User selected:", selectedColor);
+
+})
+
+//Task6
+const colorSelect = document.getElementById("color-select"); // Dropdown
+const cells = document.querySelectorAll("td"); // All table cells
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    const selectedColor = colorSelect.value; // Get selected color
+    cell.style.backgroundColor = selectedColor; // Apply color
+  });
+});
+
+
+
