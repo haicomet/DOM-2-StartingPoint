7cca2815c713b19195d90ee74d78b9de1656c471
BTBurton1 <128549369+BTBurton1@users.noreply.github.com>
2025-06-20 15:17:52 -0400

    Updated main.js to have the fillunclrdcells function

--- main.js
+++ main.js
@@ -20,4 +20,16 @@ addRowBtn.addEventListener("click", function addRow(){
     table.appendChild(newRow)
     
 })
+function fillUnclrdCells(){
+    console.log("🟡 fillUnclrdCells triggered");
+    const cells = document.querySelectorAll("td");
+    const selectedClr = document.getElementById("colorSelector").value;
+
+    cells.forEach(cell=>{
+        if(!cell.style.backgroundColor||cell.style.backgroundColor === "white" ){
+            cell.style.backgroundColor = selectedClr;
+        }
+    });
+} 
+document.getElementById("fill-grid").addEventListener("click", fillUnclrdCells);
 
