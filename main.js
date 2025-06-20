1dd8cdb675b3d3eaf82a58f56588ffc0b4be3668
BTBurton1 <128549369+BTBurton1@users.noreply.github.com>
2025-06-20 15:20:57 -0400

    Updated main.js to have the fillEveryCell function

--- main.js
+++ main.js
@@ -20,4 +20,14 @@ addRowBtn.addEventListener("click", function addRow(){
     table.appendChild(newRow)
     
 })
+function fillEveryCell(){
+    console.log("🟡 fillEveryCell triggered");
+    const cells = document.querySelectorAll("td");
+    const selectedClr = document.getElementById("colorSelector").value;
+
+    cells.forEach(cell =>{
+        cell.style.backgroundColor =selectedClr;
+    });
+}
+document.getElementById("fill-uncolored").addEventListener("click", fillEveryCell);
 
