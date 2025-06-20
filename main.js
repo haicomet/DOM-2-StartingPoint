24c7e8da51f3713fb7156b0bced6c37468908766
jeramyleon <jeramyleon@gmail.com>
2025-06-20 14:43:33 -0400

    rows can now be removed

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // Please feel free to change the JS as you see fit! This is just a starting point.
+function removeRow() {
+  const tableBody = document.getElementById('tableBody');
+  
+  try {
+    tableBody.deleteRow(0); 
+  } catch (error) {
+    console.error("No more rows to delete", error.message); 
+  }
+}
 
-const root = document.getElementById("root");
-root.addEventListener("click", (event) => {
-  console.log(event.target.tagName);
-  console.log(event.target);
-});
+const removeRowButton = document.getElementById("remove-row");
+removeRowButton.addEventListener("click", removeRow); 
+
+
