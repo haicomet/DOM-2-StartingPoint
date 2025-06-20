4a7caf3d24528adf6e15b51e7925b853577779ba
BTBurton1 <128549369+BTBurton1@users.noreply.github.com>
2025-06-20 15:06:15 -0400

    Added fillEveryCell function

---
+++ script.js
@@ -0,0 +1,9 @@
+function fillEveryCell(){
+    console.log("🟡 fillEveryCell triggered");
+    const cells = document.querySelectorAll("td");
+    const selectedClr = document.getElementById("colorSelector").value;
+
+    cells.forEach(cell =>{
+        cell.style.backgroundColor =selectedClr;
+    });
+}
